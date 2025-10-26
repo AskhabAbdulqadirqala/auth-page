a5e1e525e01784dc682bf4d5d11d799b9de94dd1
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2025-10-26 16:25:51 +0300

    init skeleton

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+    ],
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    host: true,
+    port: 3000,
+  }
+})
